64aee1b15e500d57b42ba856d834c8b79d9f3b4e
soldeitz <michele.andreata@studenti.unimi.it>
2025-02-22 11:23:28 +0100

    Refactor SCSS imports to use the new @use syntax and update webpack configuration for development server

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,29 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	entry: "./src/js/index.js",
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist/js"),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-			},
-		],
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "../css/styles.css",
-		}),
-	],
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/js"),
+    publicPath: "auto",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../css/styles.css",
+    }),
+  ],
 };
