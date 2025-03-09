20fa146fe77144eb5a24a8779e25e5f3d370c2d2
Giacomo Caironi <giacomo.caironi@gmail.com>
2025-03-09 10:30:29 +0100

    Progress, finished modal pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,24 @@
 window.onload = () => {
-	const btn_close = document.getElementsByClassName("modal-close");
-	Array.from(btn_close).forEach((elem) => {
-		elem.onclick = (_) => {
-			elem.parentElement.parentElement.style.display = "none";
-		};
-	});
 
-	window.onclick = (event) => {
-		if (event.target.classList.contains("modal")) {
-			event.target.style.display = "none";
-		}
-	};
-
-	document.getElementById("btn-sec1").onclick = (_) => {
+	document.getElementById("btn-sec1-open").onclick = (_) => {
 		document.getElementById("modal-sec1").style.display = "flex";
 	};
-	document.getElementById("btn-sec2").onclick = (_) => {
+	document.getElementById("btn-sec1-close").onclick = (_) => {
+		document.getElementById("modal-sec1").style.display = "none";
+	};
+
+
+	document.getElementById("btn-sec2-open").onclick = (_) => {
 		document.getElementById("modal-sec2").style.display = "flex";
 	};
-	document.getElementById("btn-sec3").onclick = (_) => {
+	document.getElementById("btn-sec2-close").onclick = (_) => {
+		document.getElementById("modal-sec2").style.display = "none";
+	};
+
+	document.getElementById("btn-sec3-open").onclick = (_) => {
 		document.getElementById("modal-sec3").style.display = "flex";
 	};
-	document.getElementById("btn-sec5").onclick = (_) => {
-		document.getElementById("modal-sec5").style.display = "flex";
+	document.getElementById("btn-sec3-close").onclick = (_) => {
+		document.getElementById("modal-sec3").style.display = "none";
 	};
 };
