8fe89eedfc3588faef28dd1accee5e85e0d0fd9b
Giacomo Caironi <giacomo.caironi@gmail.com>
2025-03-14 14:24:53 +0100

    Almost finished structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 window.onload = () => {
 
+	document.getElementById("pivot-link").onclick = (_) => {
+		document.getElementById("modal-sec1").style.display = "flex";
+	};
 	document.getElementById("btn-sec1-open").onclick = (_) => {
 		document.getElementById("modal-sec1").style.display = "flex";
 	};
 	document.getElementById("btn-sec1-close").onclick = (_) => {
 		document.getElementById("modal-sec1").style.display = "none";
 	};
-
 
 	document.getElementById("btn-sec2-open").onclick = (_) => {
 		document.getElementById("modal-sec2").style.display = "flex";
@@ -21,4 +23,18 @@ window.onload = () => {
 	document.getElementById("btn-sec3-close").onclick = (_) => {
 		document.getElementById("modal-sec3").style.display = "none";
 	};
+
+
+	window.onclick = (event) => {
+		if (event.target.classList.contains("sponsor-modal-inner")) {
+			event.target.parentElement.style.display = "none";
+		}
+	};
+	document.getElementById("sponsor-logo-1").onclick = (_) => {
+		document.getElementById("sponsor-1").style.display = "flex";
+	};
+	document.getElementById("btn-sponsor1-close").onclick = (_) => {
+		document.getElementById("sponsor-1").style.display = "none";
+	};
+
 };
